7cb4883ef6dfd8b1b7a8df5720c54dd066685638
rossvasquez <ross.vasquez.17@gmail.com>
2023-06-11 17:46:16 -0500

    Updates

--- src/assets/info/projects.js
+++ src/assets/info/projects.js
@@ -1,22 +1,29 @@
-
+import walletEval from '../static/walletEval.png'
+import cateringOrderSystem from '../static/cateringOrderSystem.png'
+import foundation from '../static/foundation.png'
+import zmariks from '../static/zmariks.png'
 
 const projectInfo = [
 
     {
         name: "Restaurant Site Remodel",
-        description: "Created a new website for a local business."
+        description: "Designed/Developed site for a local business.",
+        image: zmariks
     },
     {
         name: "Foundation Site",
-        description: "Created a website for a foundation supporting a late friend."
+        description: "Created a website for a foundation supporting a late friend.",
+        image: foundation
     },
     {
         name: "Mobile Coupon Wallet",
-        description: "Technical Evaluation, tasked with completing a small proof of concept mobile wallet with authentication and animation."
+        description: "Technical Evaluation, tasked with completing a small proof of concept mobile wallet with authentication and animation.",
+        image: walletEval
     },
     {
-        name: "E-Commerce Catering Order System",
-        description: "Largely my spark for web development, created an end to end catering order system to solve business problems."
+        name: "Catering Order System",
+        description: "Largely my spark for web development, created an end to end catering order system to solve business problems.",
+        image: cateringOrderSystem
     },
 
 ]
