8c3e708e00678749be08230be8c8b8abbc3beb00
rossvasquez <ross.vasquez.17@gmail.com>
2023-06-11 17:46:33 -0500

    Updates

--- src/assets/components/projects/cards.jsx
+++ src/assets/components/projects/cards.jsx
@@ -2,18 +2,23 @@ import projectInfo from '../../info/projects'
 
 function Cards() {
 
-    const mappedCards = projectInfo.map((item) => 
-        <div className='flex flex-col p-8 justify-center items-center h-auto w-auto rounded-md shadow-md bg-shallow hover:shadow-inner hover:cursor-pointer hover:border-2 border-accent'>
+    const MappedProj = () =>
+    projectInfo.map((item) => 
+        <div className='flex flex-col p-4 justify-center items-center h-auto w-auto rounded-md shadow-md bg-shallow hover:shadow-xl'>
             <div className='h-auto w-full'>
-                <p className='w-full text-center text-3xl text-white uppercase font-body font-semibold tracking-wide'>{item.name}</p>
-                <p className='w-full text-center text-2xl text-white font-body mt-4'>{item.description}</p>
+                <img className='h-auto w-full rounded-sm mb-8' src={item.image} />
+                <p className='w-full text-center text-4xl text-white uppercase font-body font-semibold tracking-wide'>{item.name}</p>
+                <div className='w-full h-[.05rem] bg-accent mt-4 opacity-80'></div>
+                <p className='w-full text-center text-2xl text-white font-body mt-4 mb-10'>{item.description}</p>
+                <div className='w-full h-16 rounded-md bg-deep flex justify-center items-center text-2xl text-white text-opacity-90 font-body font-light uppercase hover:cursor-pointer hover:text-opacity-100 hover:border border-opacity-80 border-accent'>Learn More</div>
             </div>
         </div>
+
     )
 
     return(
-        <div className='h-full w-full grid grid-cols-1 px-4 md:grid-cols-2 gap-4 md:gap-2'>
-            {mappedCards}
+        <div className='relative h-full w-full grid grid-cols-1 px-4 md:grid-cols-2 gap-4 md:gap-2'>
+            <MappedProj />
         </div>
     )
 }
--- src/assets/components/projects/index.jsx
+++ src/assets/components/projects/index.jsx
@@ -3,9 +3,9 @@ import Cards from './cards'
 function Projects() {
 
     return(
-        <div className="relative w-screen h-auto bg-deep -z-50">
+        <div className="relative w-screen h-auto bg-deep">
             <div className="w-screen max-w-7xl h-auto mx-auto pb-10">
-                <p className="pt-5 pb-8 text-accent w-full pl-0 md:pl-8 text-center md:text-left text-5xl font-bold font-header shadowSection uppercase">Projects</p>
+                <p className="pt-5 pb-8 text-accent w-full pl-0 md:pl-8 text-center md:text-left text-7xl font-bold font-header shadowSection uppercase">Projects</p>
                 <Cards />
             </div>
         </div>
